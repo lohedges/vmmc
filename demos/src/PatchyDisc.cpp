3ba84cea9f9a04066da3fafed0083cc16a570c44
Lester Hedges <lester.hedges@gmail.com>
2015-07-01 11:08:16 +0100

    Added PatchyDisc class.

---
+++ demos/src/PatchyDisc.cpp
@@ -0,0 +1,154 @@
+/*
+  Copyright (c) 2015 Lester Hedges <lester.hedges+vmmc@gmail.com>
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "PatchyDisc.h"
+
+PatchyDisc::PatchyDisc(Box& box_,
+                       std::vector<Particle>& particles_,
+                       CellList& cells_,
+                       unsigned int maxInteractions_,
+                       double interactionEnergy_,
+                       double interactionRange_) :
+    Model(box_, particles_, cells_, maxInteractions_, interactionEnergy_, interactionRange_)
+{
+    // Check dimensionality.
+    if (box.dimension != 2)
+    {
+        std::cerr << "[ERROR] PatchyDisc: Model only valid in two dimensions!\n";
+        exit(EXIT_FAILURE);
+    }
+
+    // Work out the angle between patches.
+    patchSeparation = 2.0*M_PI/maxInteractions;
+
+    // Resize rotation matrix lookup tables.
+    cosTheta.resize(maxInteractions);
+    sinTheta.resize(maxInteractions);
+
+    // Populate lookup tables.
+    for (unsigned int i=0;i<maxInteractions;i++)
+    {
+        cosTheta[i] = cos(i*patchSeparation);
+        sinTheta[i] = sin(i*patchSeparation);
+    }
+}
+
+double PatchyDisc::computePairEnergy(unsigned int particle1, double position1[],
+    double orientation1[], unsigned int particle2, double position2[], double orientation2[])
+{
+    // Separation vector.
+    std::vector<double> sep(2);
+
+    // Calculate disc separation.
+    sep[0] = position1[0] - position2[0];
+    sep[1] = position1[1] - position2[1];
+
+    // Enforce minimum image.
+    box.minimumImage(sep);
+
+    // Calculate squared norm of vector.
+    double normSqd = sep[0]*sep[0] + sep[1]*sep[1];
+
+    // Discs overlap.
+    if (normSqd < 1) return INF;
+
+    // Total interaction energy sum.
+    double energy = 0;
+
+    // Test interactions between all patch pairs.
+    for (unsigned int i=0;i<maxInteractions;i++)
+    {
+        // Compute position of patch i on first disc.
+        std::vector<double> coord1(2);
+        coord1[0] = position1[0] + 0.5*(orientation1[0]*cosTheta[i] - orientation1[1]*sinTheta[i]);
+        coord1[1] = position1[1] + 0.5*(orientation1[0]*sinTheta[i] + orientation1[1]*cosTheta[i]);
+
+        // Enforce periodic boundaries.
+        box.periodicBoundaries(coord1);
+
+        for (unsigned int j=0;j<maxInteractions;j++)
+        {
+            // Compute position of patch j on second disc.
+            std::vector<double> coord2(2);
+            coord2[0] = position2[0] + 0.5*(orientation2[0]*cosTheta[j] - orientation2[1]*sinTheta[j]);
+            coord2[1] = position2[1] + 0.5*(orientation2[0]*sinTheta[j] + orientation2[1]*cosTheta[j]);
+
+            // Enforce periodic boundaries.
+            box.periodicBoundaries(coord2);
+
+            // Calculate patch separation.
+            sep[0] = coord1[0] - coord2[0];
+            sep[1] = coord1[1] - coord2[1];
+
+            // Enforce minimum image.
+            box.minimumImage(sep);
+
+            // Calculate squared norm of vector.
+            normSqd = sep[0]*sep[0] + sep[1]*sep[1];
+
+            // Patches interact.
+            if (normSqd < squaredCutOffDistance)
+                energy -= interactionEnergy;
+        }
+    }
+
+    return energy;
+}
+
+unsigned int PatchyDisc::computeInteractions(unsigned int particle,
+    double position[], double orientation[], unsigned int interactions[])
+{
+    unsigned int cell;              // cell index
+    unsigned int neighbour;         // index of neighbouring particle
+    unsigned int nInteractions = 0; // interaction counter
+
+    // Check all neighbouring cells including same cell.
+    for (unsigned int i=0;i<cells.getNeighbours();i++)
+    {
+        cell = cells[particles[particle].cell].neighbours[i];
+
+        // Check all particles within cell.
+        for (unsigned int j=0;j<cells[cell].tally;j++)
+        {
+            neighbour = cells[cell].particles[j];
+
+            // Make sure the particles are different.
+            if (neighbour != particle)
+            {
+                // Calculate pair energy.
+                double energy = computePairEnergy(particle, position, orientation,
+                                neighbour, &particles[neighbour].position[0],
+                                &particles[neighbour].orientation[0]);
+
+                // Particles interact.
+                if (energy < 0)
+                {
+                    if (nInteractions == maxInteractions)
+                    {
+                        std::cerr << "[ERROR] PatchyDisc: Maximum number of interactions exceeded!\n";
+                        exit(EXIT_FAILURE);
+                    }
+
+                    interactions[nInteractions] = neighbour;
+                    nInteractions++;
+                }
+            }
+        }
+    }
+
+    return nInteractions;
+}
---
+++ demos/src/PatchyDisc.h
@@ -0,0 +1,96 @@
+/*
+  Copyright (c) 2015 Lester Hedges <lester.hedges+vmmc@gmail.com>
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _PATCHYDISC_H
+#define _PATCHYDISC_H
+
+#include "Model.h"
+
+/*! \file PatchyDisc.h
+*/
+
+//! Class defining the Patchy-Disc potential.
+class PatchyDisc : public Model
+{
+public:
+    //! Constructor.
+    /*! \param box_
+            A reference to the simulation box object.
+
+        \param particles_
+            A reference to the particle list.
+
+        \param cells_
+            A reference to the cell list object.
+
+        \param maxInteractions_
+            The maximum number of interactions per particle (number of patches).
+
+        \param interactionEnergy_
+            The potential energy scale (in units of kBT).
+
+        \param interactionRange_
+            The potential cut-off distance (patch diameter).
+     */
+    PatchyDisc(Box&, std::vector<Particle>&, CellList&, unsigned int, double, double);
+
+    //! Calculate the pair energy between two particles.
+    /*! \param particle1
+            The index of the first particle.
+
+        \param position1
+            The position vector of the first particle.
+
+        \param orientation1
+            The orientation vector of the first particle.
+
+        \param position2
+            The position vector of the second particle.
+
+        \param orientation2
+            The orientation vector of the second particle.
+
+        \return
+            The pair energy between particles 1 and 2.
+     */
+    double computePairEnergy(unsigned int, double[], double[], unsigned int, double[], double[]);
+
+    //! Determine the interactions for a given particle.
+    /*! \param index
+            The particle index.
+
+        \param position
+            The position vector of the particle.
+
+        \param orientation
+            The orientation vector of the particle.
+
+        \param interactions
+            An array to store the indices of neighbours with which the particle interacts.
+
+        \return
+            The number of interactions.
+     */
+     unsigned int computeInteractions(unsigned int, double[], double[], unsigned int[]);
+
+private:
+    double patchSeparation;         //!> the angle between patches in radians
+    std::vector<double> cosTheta;   //!> lookup table for cosine rotation matrix components
+    std::vector<double> sinTheta;   //!> lookup table for sine rotation matrix components
+};
+
+#endif	/* _PATCHYDISC_H */
