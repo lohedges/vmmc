ee28b18c9e5ab6500b918bd24a5a710fe91043e8
Lester Hedges <lester.hedges@gmail.com>
2015-07-08 12:23:35 +0100

    Added missing model files.

---
+++ src/Model.cpp
@@ -0,0 +1,67 @@
+/*
+  Copyright (c) 2015 Lester Hedges <lester.hedges+vmmc@gmail.com>
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "Model.h"
+
+namespace vmmc
+{
+    Model::Model() {};
+
+    Model::~Model() {};
+
+#ifndef ISOTROPIC
+    double Model::energyCallback(unsigned int particle, double position[], double orientation[])
+#else
+    double Model::energyCallback(unsigned int particle, double position[])
+#endif
+    {
+        std::cerr << "[ERROR] Model: Virtual function Model::energyCallback() must be defined.\n";
+        exit(EXIT_FAILURE);
+    }
+
+#ifndef ISOTROPIC
+    double Model::pairEnergyCallback(unsigned int, double[], double[], unsigned int, double[], double[])
+#else
+    double Model::pairEnergyCallback(unsigned int, double[], unsigned int, double[])
+#endif
+    {
+        std::cerr << "[ERROR] Model: Virtual function Model::pairEnergyCallback() must be defined.\n";
+        exit(EXIT_FAILURE);
+    }
+
+#ifndef ISOTROPIC
+    unsigned int Model::interactionsCallback(unsigned int particle,
+        double position[], double orientation[], unsigned int interactions[])
+#else
+    unsigned int Model::interactionsCallback(unsigned int particle,
+        double position[], unsigned int interactions[])
+#endif
+    {
+        std::cerr << "[ERROR] Model: Virtual function Model::interactionsCallback() must be defined.\n";
+        exit(EXIT_FAILURE);
+    }
+
+#ifndef ISOTROPIC
+    void Model::postMoveCallback(unsigned int particle, double position[], double orientation[])
+#else
+    void Model::postMoveCallback(unsigned int particle, double position[])
+#endif
+    {
+        std::cerr << "[ERROR] Model: Virtual function Model::postMoveCallback() must be defined.\n";
+        exit(EXIT_FAILURE);
+    }
+}
---
+++ src/Model.h
@@ -0,0 +1,123 @@
+/*
+  Copyright (c) 2015 Lester Hedges <lester.hedges+vmmc@gmail.com>
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _MODEL_H
+#define _MODEL_H
+
+#include <cstdlib>
+#include <iostream>
+
+/*! \file Model.h
+*/
+
+namespace vmmc
+{
+    //! Pure abstract base class defining general access functions for the model potential.
+    class Model
+    {
+    public:
+        //! Constructor.
+        Model();
+
+        //! Destructor.
+        ~Model();
+
+        //! Calculate the total interaction energy felt by a particle.
+        /*! \param index
+                The particle index.
+
+            \param position
+                The position vector of the particle.
+
+            \param orientation
+                The orientation vector of the first particle.
+
+            \return
+                The total interaction energy.
+        */
+#ifndef ISOTROPIC
+        virtual double energyCallback(unsigned int, double[], double[]);
+#else
+        virtual double energyCallback(unsigned int, double[]);
+#endif
+
+        //! Calculate the pair energy between two particles.
+        /*! \param particle1
+                The index of the first particle.
+
+            \param position1
+                The position vector of the first particle.
+
+            \param orientation1
+                The orientation vector of the first particle.
+
+            \param position2
+                The position vector of the second particle.
+
+            \param orientation2
+                The orientation vector of the second particle.
+
+            \return
+                The pair energy between particles 1 and 2.
+        */
+#ifndef ISOTROPIC
+        virtual double pairEnergyCallback(unsigned int, double[], double[], unsigned int, double[], double[]);
+#else
+        virtual double pairEnergyCallback(unsigned int, double[], unsigned int, double[]);
+#endif
+
+        //! Determine the interactions for a given particle.
+        /*! \param index
+                The particle index.
+
+            \param position
+                The position vector of the particle.
+
+            \param orientation
+                The orientation vector of the particle.
+
+            \param interactions
+                An array to store the indices of neighbours with which the particle interacts.
+
+            \return
+                The number of interactions.
+        */
+#ifndef ISOTROPIC
+        virtual unsigned int interactionsCallback(unsigned int, double[], double[], unsigned int[]);
+#else
+        virtual unsigned int interactionsCallback(unsigned int, double[], unsigned int[]);
+#endif
+
+        //! Apply any post-move updates for a given particle.
+        /*! \param index
+                The particle index.
+
+            \param position
+                The position of the particle following the virtual move.
+
+            \param orientation
+                The orientation of the particle following the virtual move.
+        */
+#ifndef ISOTROPIC
+        virtual void postMoveCallback(unsigned int, double[], double[]);
+#else
+        virtual void postMoveCallback(unsigned int, double[]);
+#endif
+    };
+}
+
+#endif	/* _MODEL_H */
