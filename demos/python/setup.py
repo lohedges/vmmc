97bbef37acac28cfe347bfc392b75cf5c6a3460d
Lester Hedges <lester.hedges@gmail.com>
2015-08-13 21:50:19 +0100

    Added demo code using Python C API.

---
+++ demos/python/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='demo',
+      description='Demo library for linking Python to LibVMMC',
+      url='http://github.com/lohedges/vmmc',
+      author='Lester Hedges',
+      author_email='lester.hedges+vmmc@gmail.com',
+      license='GPLv3+',
+      packages=['demo'])
