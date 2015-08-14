e0c89544edad22db9b9d06ea9ab8ff636811066f
Lester Hedges <lester.hedges@gmail.com>
2015-08-14 09:48:40 +0100

    Fixed indentation.

--- demos/python/demo/squarewellium.py
+++ demos/python/demo/squarewellium.py
@@ -21,20 +21,20 @@ class SquareWellium(Model):
         # Call base class constructor.
         Model.__init__(self, box, particles, cells, max_interactions, interaction_energy, interaction_range)
 
-    def compute_pair_energy(self, particle1, position1, orientation1,
-            particle2, position2, orientation2):
+    def compute_pair_energy(self, particle1, position1, orientation1, particle2, position2, orientation2):
+        """ Compute pair interaction energy between two particles. """
 
-            # Calculate particle separation.
-            sep = map(sub, position1, position2)
+        # Calculate particle separation.
+        sep = map(sub, position1, position2)
 
-            # Compute minimum image.
-            self.box.minimum_image(sep)
+        # Compute minimum image.
+        self.box.minimum_image(sep)
 
-            # Compute squared norm of separation vector.
-            norm_sqd = sum(map(mul, sep, sep))
+        # Compute squared norm of separation vector.
+        norm_sqd = sum(map(mul, sep, sep))
 
-            if norm_sqd < 1:
-                return float("inf")
-            if norm_sqd < self.squared_cutoff_distance:
-                return -self.interaction_energy
-            return 0
+        if norm_sqd < 1:
+            return float("inf")
+        if norm_sqd < self.squared_cutoff_distance:
+            return -self.interaction_energy
+        return 0
